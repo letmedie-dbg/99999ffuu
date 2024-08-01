dcf9866cac3138212a9ec5e86945050fb94fe7f7
letmedie-dbg <outk3118@gmail.com>
2024-08-01 18:49:33 +0300

    Update updateoffsets.cmd

--- updateoffsets.cmd
+++ updateoffsets.cmd
@@ -5,4 +5,4 @@ curl https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/buttons.json -
 curl https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client_dll.json -o client.dll.json
 curl https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json -o offsets.json
 echo Dosyalar indirildi.
-pause
+exit
