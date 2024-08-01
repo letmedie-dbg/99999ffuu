646d601a69b311dc5c3f1e32ee963c33e7176967
letmedie-dbg <outk3118@gmail.com>
2024-08-01 18:41:15 +0300

    Update updateoffsets.cmd

--- updateoffsets.cmd
+++ updateoffsets.cmd
@@ -1,4 +1,6 @@
 @echo off
+set "current_dir=%~dp0"
+cd /d "%current_dir%"
 curl https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/buttons.json -o buttons.json
 curl https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client_dll.json -o client.dll.json
 curl https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json -o offsets.json
